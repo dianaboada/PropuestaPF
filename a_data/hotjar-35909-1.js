e0910d8957bf14032bfd0f38531a3a8ce2cf739d
Megami <Megami@MacBook-Pro-de-Megami.local>
2017-09-13 16:56:51 -0500

    Pagina PF Version original c
    cz
    ccc

---
+++ a_data/hotjar-35909-1.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":35909,"record_targeting_rules":[],"forms":[],"record":false,"r":0.1208951422,"deferred_page_contents":[]};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-84b5559189f3c7aa75bf6be523affb75.js');
---
+++ carrito_data/hotjar-35909.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":35909,"record_targeting_rules":[],"forms":[],"record":false,"r":0.0611845326,"deferred_page_contents":[]};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-9e963b8c2870c8ee7ef25bf04228c790.js');
